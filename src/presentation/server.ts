f1b6557253f1fae14d9dfc6a7d3b30ccfc01f58c
Carlos Motiño <carlos.motino@alcon.com>
2023-12-22 10:03:22 -0600

    fin seccion 8

---
+++ src/presentation/server.ts
@@ -0,0 +1,23 @@
+import { CronJob } from "cron"
+import { CronService } from "./cron/cron-service";
+import { CheckService } from "../domain/use-cases/checks/check-service";
+
+export class Server {
+    /**
+     * start
+     */
+    public static  start() {
+        console.log('Server started...');
+
+        CronService.createJob(
+            '*/5 * * * * *',
+            () => {
+                new CheckService(
+                    () => console.log('success'),
+                    ( error ) => console.log(error),
+                ).execute('https://google.com')
+                //new CheckService().execute('http://localhost:3000')
+            }
+        );
+    }
+}
